d721f78eb6bfa0de5b1a30ed8b46d4d34e37f5c9
christina bartholomæussen <c.m.bartholo@gmail.com>
2021-10-11 12:58:01 +0200

    Add project files.

---
+++ City.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace postnumre_csv
+{
+    class City
+    {
+        public string Postnummer { get; set; }
+        public string By { get; set; }
+
+        public City(string rowData)
+        {
+            string[] data = rowData.Split(';');
+
+            this.Postnummer = data[0];
+            this.By = data[1];
+        }
+
+        public override string ToString()
+        {
+
+            string str = $"Postnummer: {Postnummer} By: {By}";
+
+            return str;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace postnumre_csv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] linesInCsv = File.ReadAllLines("danske-postnumre-byer-ny.csv");
+
+            var cities = new List<City>();
+
+  
+           for(int i = 1; i < linesInCsv.Length; i++)
+           {
+               City city = new City(linesInCsv[i]);
+               cities.Add(city);
+
+           }
+
+            do
+            {
+
+                Console.Write("Indtast dit postnummer: ");
+                string input = Console.ReadLine();
+
+                var match = cities.FirstOrDefault(c => c.Postnummer == input);
+     
+                if (match == null)
+                {
+                    Console.WriteLine("Intet fundet...");
+                    
+                }
+                else
+                {
+                    Console.WriteLine(match);
+                }
+
+            } while (Console.ReadKey().KeyChar != 'x');
+
+        }
+    }
+}
